580a4ad816c08388f962e3397c8de8ed4e3d5d92
KsenijaCode <Rocking@email.com>
2024-10-07 14:50:42 +0300

    #12-#13 mixed. Needs fixing

--- cypress/e2e/AddToCart.cy.js
+++ cypress/e2e/AddToCart.cy.js
@@ -8,29 +8,28 @@ describe('Add Products in Cart', () => {
     // Step 2: Verify that home page is visible successfully
     cy.get('h1').should('be.visible');
 
-    // Step 3: Click 'Products' button
+    // Step 3: Add products to cart
     cy.contains('Products').click();
 
-    // Step 4: Hover over the first product and click 'Add to cart'
-    cy.get('.product-image-wrapper').first().trigger('mouseover');
-    cy.contains('Add to cart').first().click();
+    const quantityToAdd = 3;    
+    cy.addProductToCart(quantityToAdd);
 
-    // Step 5: Click 'Continue Shopping' button
-    cy.contains('Continue Shopping').click();
+    // Verify that product is added to the cart
+    cy.get('#cartModal').should('contain', 'Added');
 
-    // Step 6: Hover over the second product and click 'Add to cart'
-    cy.get('.product-image-wrapper').eq(1).trigger('mouseover');
-    cy.contains('Add to cart').eq(1).click();
+    // Step 4: Click 'Cart' button
+    cy.contains('Cart').click();
 
-    // Step 7: Click 'View Cart' button
-    cy.contains('View Cart').click();
+    // Step 5: Verify that cart page is displayed
+    cy.url().should('include', 'view_cart');
+    cy.get('.active').contains('Shopping Cart').should('be.visible');
 
-    // Step 8: Verify both products are added to Cart
-    cy.get('.cart_description').should('have.length', 2);
+    // Step 6: Verify all products are added to Cart
+    cy.get('.cart_menu').should('have.length', 3);
 
-    // Step 9: Verify their prices, quantity, and total price
-    cy.get('.cart_description').eq(0).within(() => {
-      cy.get('.product_name').should('be.visible');
+    // Step 7: Verify their prices, quantity, and total price
+      cy.get('.cart_menu').eq(3).within(() => {
+      cy.get('.cart_product').should('be.visible');
       cy.get('.cart_price').should('be.visible');
       cy.get('.cart_quantity input').should('have.value', '1');
     });
